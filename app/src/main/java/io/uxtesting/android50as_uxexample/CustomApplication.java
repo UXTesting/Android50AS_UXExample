145c794d7175422a96f642da90d625de3460239d
Morshues <morshues@gmail.com>
2016-03-01 13:58:21 +0800

    Change auto upload mode to auto.

--- app/src/main/java/io/uxtesting/android50as_uxexample/CustomApplication.java
+++ app/src/main/java/io/uxtesting/android50as_uxexample/CustomApplication.java
@@ -3,6 +3,7 @@ package io.uxtesting.android50as_uxexample;
 import android.app.Application;
 
 import io.uxtesting.UXTesting;
+import io.uxtesting.UploadMode;
 
 public class CustomApplication extends Application {
 
@@ -11,6 +12,7 @@ public class CustomApplication extends Application {
         super.onCreate();
 
         UXTesting.Init(this, "MY_APP_KEY");
+        UXTesting.setAutoUploadMode(UploadMode.AUTO);
     }
 
 }
